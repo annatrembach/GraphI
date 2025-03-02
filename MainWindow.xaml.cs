027a21ab8165111d0b3f3585790e225f94c2b86c
Anna <trembach.ann@gmail.com>
2025-03-02 15:20:35 +0200

    added font size control elements and command binding

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            CommandBinding saveBinding = new CommandBinding(
+                ApplicationCommands.Save, Save_Executed, Save_CanExecute);
+            this.CommandBindings.Add(saveBinding);
+
+            CommandBinding openBinding = new CommandBinding(
+                ApplicationCommands.Open, Open_Executed, Open_CanExecute);
+            this.CommandBindings.Add(openBinding);
+
+            CommandBinding deleteBinding = new CommandBinding(
+                ApplicationCommands.Delete, Delete_Executed, Delete_CanExecute);
+            this.CommandBindings.Add(deleteBinding);
+
+        }
+
+        private void Save_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Команда Save виконана.");
+        }
+
+        private void Open_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Команда Open виконана.");
+        }
+
+        private void Delete_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void Delete_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Команда Delete виконана.");
         }
     }
 }
